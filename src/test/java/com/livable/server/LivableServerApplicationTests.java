6231f2242e82aa50c065ed9aa648207b84cade69
khsrla9806 <kimhunsope@naver.com>
2023-09-04 17:10:41 +0900

    Feat: 프로젝트 세팅 (#3)

---
+++ src/test/java/com/livable/server/LivableServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livable.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivableServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
