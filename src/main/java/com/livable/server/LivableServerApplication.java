23b5eb86605da0b22884811fc1f4f592508aed14
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-09-04 17:14:54 +0900

    프로젝트 세팅 완료 (#4)

---
+++ src/main/java/com/livable/server/LivableServerApplication.java
@@ -0,0 +1,13 @@
+package com.livable.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivableServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivableServerApplication.class, args);
+    }
+
+}
