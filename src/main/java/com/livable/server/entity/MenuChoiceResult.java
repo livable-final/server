3ec445927ce8fa2bcacddabca94ccbaa36e9c069
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-08 18:38:04 +0900

    Feat: MenuChoiceResult 엔티티 클래스 생성 (#5)

---
+++ src/main/java/com/livable/server/entity/MenuChoiceResult.java
@@ -0,0 +1,42 @@
+package com.livable.server.entity;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "menu_choice_result",
+        uniqueConstraints =
+        @UniqueConstraint(
+                name = "MEMBER_BUILDING_DATE_UNIQUE_IDX",
+                columnNames = {"building_id", "menu_id", "date"}
+        )
+)
+@Entity
+public class MenuChoiceResult extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Building building;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Menu menu;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Integer count;
+}
