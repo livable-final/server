ab05326c85697fd5564e8c74f7eb75579dcd6212
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-08 18:37:27 +0900

    Feat: LunchBoxReview 엔티티 클래스 생성 (#5)

---
+++ src/main/java/com/livable/server/entity/LunchBoxReview.java
@@ -0,0 +1,24 @@
+package com.livable.server.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class LunchBoxReview extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @Column(nullable = false)
+    private String description;
+}
