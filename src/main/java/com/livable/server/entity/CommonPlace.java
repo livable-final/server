3f849a8757d6307dd69ad01406fdf2ea65e4ebb9
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-08 18:33:44 +0900

    Feat: CommonPlace 엔티티 클래스 생성 (#5)

---
+++ src/main/java/com/livable/server/entity/CommonPlace.java
@@ -0,0 +1,27 @@
+package com.livable.server.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class CommonPlace extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String floor;
+
+    private String roomNumber;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Building building;
+}
