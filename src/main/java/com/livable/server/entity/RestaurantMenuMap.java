ced652468e076be5beb76d5a58405917242b316c
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-08 18:38:45 +0900

    Feat: RestaurantMenuMap 엔티티 클래스 생성 (#5)

---
+++ src/main/java/com/livable/server/entity/RestaurantMenuMap.java
@@ -0,0 +1,33 @@
+package com.livable.server.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "restaurant_menu_map",
+        uniqueConstraints =
+        @UniqueConstraint(
+                name = "RESTAURANT_MENU_UNIQUE_IDX",
+                columnNames = {"restaurant_id", "menu_id"}
+        )
+)
+@Entity
+public class RestaurantMenuMap extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Restaurant restaurant;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Menu menu;
+}
