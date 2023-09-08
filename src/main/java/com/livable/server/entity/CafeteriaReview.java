56d337c29c0d698d5d72571ccc3d32702fe9e6f1
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-08 18:33:23 +0900

    Feat: CafeteriaReview 엔티티 클래스 생성 (#5)

---
+++ src/main/java/com/livable/server/entity/CafeteriaReview.java
@@ -0,0 +1,30 @@
+package com.livable.server.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class CafeteriaReview extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Building building;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Evaluation taste;
+
+}
