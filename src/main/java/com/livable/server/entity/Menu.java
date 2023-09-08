bb8668686b0b195d6793f2fdabe594c111464792
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-08 18:37:48 +0900

    Feat: Menu 엔티티 클래스 생성 (#5)

---
+++ src/main/java/com/livable/server/entity/Menu.java
@@ -0,0 +1,21 @@
+package com.livable.server.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Menu extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Category category;
+}
