5c091eeb054eb7d29d75c64913195464f62a6b3c
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-08 18:37:17 +0900

    Feat: InvitationReservationMap 엔티티 클래스 생성 (#5)

---
+++ src/main/java/com/livable/server/entity/InvitationReservationMap.java
@@ -0,0 +1,25 @@
+package com.livable.server.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class InvitationReservationMap extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Invitation invitation;
+
+    @JoinColumn(nullable = false, unique = true)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Reservation reservation;
+}
