1bd0f0ae92608272a431b583da338e0207e79617
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-08 18:57:42 +0900

    Modify: BuildingRestaurantMap 복합 유니크 인덱스 추가 (#5)
    Building Restaurant 복합 유니크 인덱스 설정

--- src/main/java/com/livable/server/entity/BuildingRestaurantMap.java
+++ src/main/java/com/livable/server/entity/BuildingRestaurantMap.java
@@ -8,6 +8,14 @@ import javax.persistence.*;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "building_restaurant_map",
+        uniqueConstraints =
+        @UniqueConstraint(
+                name = "BUILDING_RESTAURANT_UNIQUE_IDX",
+                columnNames = {"building_id", "restaurant_id"}
+        )
+)
 @Entity
 public class BuildingRestaurantMap extends BaseTimeEntity {
 
