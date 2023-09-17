3613f6d467029a07dbcdfe44ac0f2d96e5da30bf
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-18 00:39:01 +0900

    Feat: 초대장 시작 및 종료 시간을 가져오는 메서드 구현 (#22)
    QR 생성 및 유효성 검사를 위해 초대장 시작 및 종료 시간을 조회

---
+++ src/main/java/com/livable/server/visitation/service/InvitationService.java
@@ -0,0 +1,29 @@
+package com.livable.server.visitation.service;
+
+import com.livable.server.core.exception.GlobalRuntimeException;
+import com.livable.server.entity.Invitation;
+import com.livable.server.visitation.domain.VisitationErrorCode;
+import com.livable.server.visitation.dto.VisitationResponse;
+import com.livable.server.visitation.repository.InvitationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class InvitationService {
+
+    private final InvitationRepository invitationRepository;
+
+    public VisitationResponse.InvitationTimeDto findInvitationTime(Long invitationId) {
+        Invitation invitation = invitationRepository.findById(invitationId)
+                .orElseThrow(() -> new GlobalRuntimeException(VisitationErrorCode.NOT_FOUND));
+
+        return VisitationResponse.InvitationTimeDto
+                .builder()
+                .startTime(invitation.getStartTime())
+                .endTime(invitation.getEndTime())
+                .startDate(invitation.getStartDate())
+                .endDate(invitation.getEndDate())
+                .build();
+    }
+}
