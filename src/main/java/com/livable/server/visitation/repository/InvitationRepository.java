bd19dac4600c5568b8a0d43dbc2587d09d9cecf1
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-18 00:47:42 +0900

    Modify: InvitationRepository 메서드 제거 (#22)
    사용하지 않는 불필요한 메서드 제거

--- src/main/java/com/livable/server/visitation/repository/InvitationRepository.java
+++ src/main/java/com/livable/server/visitation/repository/InvitationRepository.java
@@ -4,5 +4,4 @@ import com.livable.server.entity.Invitation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvitationRepository extends JpaRepository<Invitation, Long> {
-    Invitation findBy(Long visitorId);
 }
