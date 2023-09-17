6810b07fce8c95031cae4c31afb7613ad8be7263
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-18 00:33:31 +0900

    Feat: 필요한 Repository 생성 (#22)
    Invitation, Visitor Repository 생성

---
+++ src/main/java/com/livable/server/visitation/repository/InvitationRepository.java
@@ -0,0 +1,8 @@
+package com.livable.server.visitation.repository;
+
+import com.livable.server.entity.Invitation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvitationRepository extends JpaRepository<Invitation, Long> {
+    Invitation findBy(Long visitorId);
+}
