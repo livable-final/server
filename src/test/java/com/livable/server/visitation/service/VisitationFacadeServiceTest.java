12c0a32956b38210bfed6d58826246e67d33ecd7
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-18 17:16:31 +0900

    Test: VisitationFacadeService.createQrCode 성공 테스트 구현 (#22)

---
+++ src/test/java/com/livable/server/visitation/service/VisitationFacadeServiceTest.java
@@ -0,0 +1,63 @@
+package com.livable.server.visitation.service;
+
+import com.livable.server.visitation.dto.VisitationResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class VisitationFacadeServiceTest {
+
+    private final static String QR_CODE = "QR_CODE";
+
+    @InjectMocks
+    VisitationFacadeService visitationFacadeService;
+
+    @Mock
+    VisitationService visitationService;
+    @Mock
+    InvitationService invitationService;
+    @Mock
+    VisitorService visitorService;
+
+    @DisplayName("VisitationFacadeService.createQrCode 성공 테스트")
+    @Test
+    void createQrCodeSuccessTest() {
+        // Given
+        VisitationResponse.InvitationTimeDto invitationTimeDto = VisitationResponse.InvitationTimeDto.builder()
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now())
+                .startTime(LocalTime.now())
+                .endTime(LocalTime.now())
+                .build();
+
+        given(visitorService.findInvitationId(anyLong())).willReturn(1L);
+        given(invitationService.findInvitationTime(anyLong())).willReturn(invitationTimeDto);
+        given(visitationService.createQrCode(any(LocalDateTime.class), any(LocalDateTime.class))).willReturn(QR_CODE);
+
+        // When
+        String qrCode = visitationFacadeService.createQrCode(1L);
+
+        // Then
+        assertThat(qrCode).isEqualTo(QR_CODE);
+        then(visitorService).should(times(1)).findInvitationId(anyLong());
+        then(invitationService).should(times(1)).findInvitationTime(anyLong());
+        then(visitationService).should(times(1)).createQrCode(any(LocalDateTime.class), any(LocalDateTime.class));
+    }
+
+}
