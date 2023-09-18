089b5f39d901aac2f97434cb6fd1222509c29bc6
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-18 16:26:32 +0900

    Test: VisitorService.findInvitationId 실패 테스트 구현 (#22)

--- src/test/java/com/livable/server/visitation/service/VisitorServiceTest.java
+++ src/test/java/com/livable/server/visitation/service/VisitorServiceTest.java
@@ -1,7 +1,9 @@
 package com.livable.server.visitation.service;
 
+import com.livable.server.core.exception.GlobalRuntimeException;
 import com.livable.server.entity.Invitation;
 import com.livable.server.entity.Visitor;
+import com.livable.server.visitation.domain.VisitationErrorCode;
 import com.livable.server.visitation.repository.VisitorRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,9 +15,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 class VisitorServiceTest {
@@ -46,6 +50,29 @@ class VisitorServiceTest {
 
         // Then
         assertThat(invitationId).isEqualTo(1L);
+        then(visitorRepository).should(times(1)).findById(anyLong());
     }
 
+    @DisplayName("VisitorService.findInvitationId 실패 테스트")
+    @Test
+    void findInvitationIdFailTest() {
+        // Given
+        Invitation invitation = Invitation.builder()
+                .id(1L)
+                .build();
+
+        Visitor visitor = Visitor.builder()
+                .name("태윤초이")
+                .invitation(invitation)
+                .build();
+
+        given(visitorRepository.findById(anyLong())).willReturn(Optional.empty());
+
+        // When
+        GlobalRuntimeException globalRuntimeException = assertThrows(GlobalRuntimeException.class, () -> visitorService.findInvitationId(1L));
+
+        // Then
+        assertThat(globalRuntimeException.getErrorCode()).isEqualTo(VisitationErrorCode.NOT_FOUND);
+        then(visitorRepository).should(times(1)).findById(anyLong());
+    }
 }
