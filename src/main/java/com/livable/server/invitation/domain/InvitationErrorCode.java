627def108cecad0c2c1bd712b8f6a65b3d5522cc
khsrla9806 <kimhunsope@naver.com>
2023-09-19 00:36:31 +0900

    Feat: 초대장 저장 Service 로직에서 발생할 수 있는 에러 코드 구현 (#31)

--- src/main/java/com/livable/server/invitation/domain/InvitationErrorCode.java
+++ src/main/java/com/livable/server/invitation/domain/InvitationErrorCode.java
@@ -9,7 +9,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum InvitationErrorCode implements ErrorCode {
-    MEMBER_NOT_EXIST(HttpStatus.BAD_REQUEST, "존재하지 않는 회원 정보입니다.");
+    MEMBER_NOT_EXIST(HttpStatus.BAD_REQUEST, "존재하지 않는 회원 정보입니다."),
+    INVALID_INTERVIEW_MAXIMUM_NUMBER(HttpStatus.BAD_REQUEST, "면접 초대 가능 인원수는 1명입니다."),
+    INVALID_DATE(HttpStatus.BAD_REQUEST, "종료 날짜가 시작 날짜보다 과거일 수 없습니다."),
+    INVALID_TIME(HttpStatus.BAD_REQUEST, "종료 시간이 시작 시간보다 과거일 수 없습니다."),
+    INVALID_RESERVATION_COUNT(HttpStatus.BAD_REQUEST, "해당 날짜 또는 시간에 예약된 장소 정보가 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
