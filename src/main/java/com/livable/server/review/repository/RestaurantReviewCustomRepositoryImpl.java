302b7118b3509e5bb2a1863d643e6c3f3d0a8fea
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-09-18 23:50:56 +0900

    Feat: 특정 메뉴에 대한 리뷰 리스트 레포지토리 QueryDSL 로직 구현 (#33)

--- src/main/java/com/livable/server/review/repository/RestaurantReviewCustomRepositoryImpl.java
+++ src/main/java/com/livable/server/review/repository/RestaurantReviewCustomRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.livable.server.review.repository;
 
 import com.livable.server.entity.*;
 import com.livable.server.review.dto.RestaurantReviewResponse;
+import com.querydsl.core.QueryFactory;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -12,6 +13,12 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+
+import static com.livable.server.entity.QMember.member;
+import static com.livable.server.entity.QRestaurant.restaurant;
+import static com.livable.server.entity.QRestaurantReview.restaurantReview;
+import static com.livable.server.entity.QReview.review;
+import static com.livable.server.entity.QReviewMenuMap.reviewMenuMap;
 
 @RequiredArgsConstructor
 public class RestaurantReviewCustomRepositoryImpl implements RestaurantReviewCustomRepository {
@@ -54,6 +61,45 @@ public class RestaurantReviewCustomRepositoryImpl implements RestaurantReviewCus
                 .orderBy(review.createdAt.desc());
 
         List<RestaurantReviewResponse.ListDTO> content = query
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchJoin().fetch();
+
+        long total = query.fetchCount();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public Page<RestaurantReviewResponse.ListForMenuDTO> findRestaurantReviewByMenuId(Long menuId, Pageable pageable) {
+
+        JPAQuery<RestaurantReviewResponse.ListForMenuDTO> query = queryFactory
+                .select(Projections.constructor(RestaurantReviewResponse.ListForMenuDTO.class,
+                        review.id,
+                        review.createdAt,
+                        review.description,
+                        restaurantReview.taste,
+                        restaurantReview.amount,
+                        restaurantReview.service,
+                        restaurantReview.speed,
+                        restaurantReview.restaurant.id,
+                        restaurant.name,
+                        review.member.id,
+                        member.name
+                ))
+                .from(review)
+                .innerJoin(restaurantReview).on(restaurantReview.id.eq(review.id))
+                .innerJoin(member).on(review.member.id.eq(member.id))
+                .innerJoin(restaurant).on(restaurantReview.restaurant.id.eq(restaurant.id))
+                .where(review.id.in(
+                        JPAExpressions
+                                .select(reviewMenuMap.review.id)
+                                .from(reviewMenuMap)
+                                .where(reviewMenuMap.menu.id.eq(menuId))
+                ))
+                .orderBy(review.createdAt.desc());
+
+        List<RestaurantReviewResponse.ListForMenuDTO> content = query
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetchJoin().fetch();
