8031ed0fd9cc47562bebbde579e71567f61a023c
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-18 16:18:30 +0900

    Test: VisitorService.findInvitationId 성공 테스트 구현 (#22)

---
+++ src/test/java/com/livable/server/visitation/service/VisitorServiceTest.java
@@ -0,0 +1,51 @@
+package com.livable.server.visitation.service;
+
+import com.livable.server.entity.Invitation;
+import com.livable.server.entity.Visitor;
+import com.livable.server.visitation.repository.VisitorRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class VisitorServiceTest {
+
+    @InjectMocks
+    VisitorService visitorService;
+
+    @Mock
+    VisitorRepository visitorRepository;
+
+    @DisplayName("VisitorService.findInvitationId 성공 테스트")
+    @Test
+    void findInvitationIdSuccessTest() {
+        // Given
+        Invitation invitation = Invitation.builder()
+                .id(1L)
+                .build();
+
+        Visitor visitor = Visitor.builder()
+                .name("태윤초이")
+                .invitation(invitation)
+                .build();
+
+        given(visitorRepository.findById(anyLong())).willReturn(Optional.of(visitor));
+
+        // When
+        Long invitationId = visitorService.findInvitationId(1L);
+
+        // Then
+        assertThat(invitationId).isEqualTo(1L);
+    }
+
+}
