ada7371341c388969706e725aebddf2dd0995fd4
khsrla9806 <kimhunsope@naver.com>
2023-09-19 00:35:58 +0900

    Feat: 초대장 저장에 필요한 Repository 구현 및 쿼리 메서드 작성 (#31)

---
+++ src/main/java/com/livable/server/invitation/repository/InvitationReservationMapRepository.java
@@ -0,0 +1,7 @@
+package com.livable.server.invitation.repository;
+
+import com.livable.server.entity.InvitationReservationMap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvitationReservationMapRepository extends JpaRepository<InvitationReservationMap, Long> {
+}
