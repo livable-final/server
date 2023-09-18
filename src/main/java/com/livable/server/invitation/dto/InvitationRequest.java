2df0c15040e92f0dbc67c4120491812aad5cf10f
khsrla9806 <kimhunsope@naver.com>
2023-09-19 02:25:50 +0900

    Feat: 초대장 생성 DTO 유효성 검사 메시지, 코드 추가 (#31)

--- src/main/java/com/livable/server/invitation/dto/InvitationRequest.java
+++ src/main/java/com/livable/server/invitation/dto/InvitationRequest.java
@@ -3,11 +3,18 @@ package com.livable.server.invitation.dto;
 import com.livable.server.entity.Invitation;
 import com.livable.server.entity.Member;
 import com.livable.server.entity.Visitor;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import static com.livable.server.invitation.domain.InvitationValidationMessage.*;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class InvitationRequest {
@@ -16,21 +23,25 @@ public class InvitationRequest {
     @Builder
     public static class CreateDTO {
 
-        @NotNull
+        @NotNull(message = NOT_NULL)
         private String purpose;
 
         private Long commonPlaceId;
 
-        @NotNull
+        @NotNull(message = NOT_NULL)
         private String officeName;
 
         private String description;
 
-        @NotNull
+        @NotNull(message = NOT_NULL)
+        @FutureOrPresent(message = REQUIRED_FUTURE_DATE)
         private LocalDateTime startDate;
 
-        @NotNull
+        @NotNull(message = NOT_NULL)
+        @FutureOrPresent(message = REQUIRED_FUTURE_DATE)
         private LocalDateTime endDate;
+
+        @Size(min = 1, max = 30, message = REQUIRED_VISITOR_COUNT)
         private List<VisitorCreateDTO> visitors;
 
         public Invitation toEntity(Member member) {
