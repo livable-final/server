dc1567fb363e5e1df4682457a2a66bdef3d920ab
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-18 16:26:08 +0900

    Style: 불필요한 Import문 제거 (#22)
    InvitationServiceTest 내부에 MockBean import문 제거

--- src/test/java/com/livable/server/visitation/service/InvitationServiceTest.java
+++ src/test/java/com/livable/server/visitation/service/InvitationServiceTest.java
@@ -11,15 +11,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
