f3e4a9103e0c2573b409b7721d7a33434bc0de39
정현수 <junghss0113@gmail.com>
2023-09-19 19:10:00 +0900

    Amazon S3 적용 및 이미지 저장 기능 구현 완료 (#49)
    * Chore: Amazon S3 의존성 추가 (#40)
    
    * Feat: Amazon S3 연결을 위한 설정 클래스 구현 (#40)
    
    * Feat: Amazon S3 파일 저장 컴포넌트 구현 (#40)

---
+++ src/main/java/com/livable/server/core/util/S3Uploader.java
@@ -0,0 +1,75 @@
+package com.livable.server.core.util;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class S3Uploader {
+
+    private static final List<String> ALLOWED_EXTENSIONS = List.of("jpg", "png", "gif", "jpeg");
+    private static final String EXTENSION_SEPARATOR = ".";
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public List<String> saveFile(List<MultipartFile> files) throws IOException {
+
+        List<String> accessUrls = new ArrayList<>();
+
+        for (MultipartFile file : files) {
+            String accessUrl = saveFile(file);
+            accessUrls.add(accessUrl);
+        }
+        return accessUrls;
+    }
+
+    public String saveFile(MultipartFile file) throws IOException {
+
+        final String originalFileName = file.getOriginalFilename();
+        assert originalFileName != null;
+        final String fileExtension = getFileExtension(originalFileName);
+
+        validationAllowedFileExtension(fileExtension);
+        String randomFileName = generateRandomFileName(originalFileName, fileExtension);
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType());
+
+        amazonS3.putObject(bucket, randomFileName, file.getInputStream(), metadata);
+
+        return amazonS3.getUrl(bucket, randomFileName).toString();
+    }
+
+    // 랜덤한 파일 이름을 생성하는 메서드
+    private String generateRandomFileName(String originalFileName, String fileExtension) {
+        return UUID.randomUUID() + originalFileName + EXTENSION_SEPARATOR + fileExtension;
+    }
+
+    // 파일 이름에서 파일 확장자를 추출하는 메서드
+    private String getFileExtension(String originalFileName) {
+        return originalFileName
+                .substring(originalFileName.lastIndexOf(EXTENSION_SEPARATOR) + 1)
+                .toLowerCase();
+
+    }
+
+    // 파일 확장자를 검증하는 메서드
+    private void validationAllowedFileExtension(String fileExtension) {
+        if (!ALLOWED_EXTENSIONS.contains(fileExtension)) {
+            throw new IllegalArgumentException("지원하지 않는 파일 확장자 입니다.");
+        }
+    }
+}
