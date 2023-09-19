364fc6d29fcd7df2e2fcc137f3f6adfd0b39f582
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-19 16:23:21 +0900

    Feat: Validation Error Message 추가 (#28)

--- src/main/java/com/livable/server/visitation/dto/VisitationRequest.java
+++ src/main/java/com/livable/server/visitation/dto/VisitationRequest.java
@@ -1,5 +1,6 @@
 package com.livable.server.visitation.dto;
 
+import com.livable.server.invitation.domain.InvitationValidationGuideMessage;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +17,7 @@ public class VisitationRequest {
 
     @Getter
     public static class RegisterParkingDto {
-        @Pattern(regexp = "^\\d{2,3}[가-힣]{1}\\d{4}$")
+        @Pattern(regexp = "^\\d{2,3}[가-힣]{1}\\d{4}$", message = InvitationValidationGuideMessage.INVALID_CAR_NUMBER)
         private String carNumber;
     }
 }
