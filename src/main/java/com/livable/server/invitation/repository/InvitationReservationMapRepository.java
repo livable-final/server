3db9564fa5041efc97f130f073f32dbcf629633f
khsrla9806 <kimhunsope@naver.com>
2023-09-20 17:27:57 +0900

    Feat: 연관된 예약 정보, 방문자, 주차 정보 삭제 쿼리 메서드 구현 (#57)

--- src/main/java/com/livable/server/invitation/repository/InvitationReservationMapRepository.java
+++ src/main/java/com/livable/server/invitation/repository/InvitationReservationMapRepository.java
@@ -2,6 +2,12 @@ package com.livable.server.invitation.repository;
 
 import com.livable.server.entity.InvitationReservationMap;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface InvitationReservationMapRepository extends JpaRepository<InvitationReservationMap, Long> {
+
+    @Modifying
+    @Query("delete from InvitationReservationMap irm where irm.invitation.id = :invitationId")
+    void deleteAllByInvitationId(Long invitationId);
 }
--- src/main/java/com/livable/server/visitation/repository/ParkingLogRepository.java
+++ src/main/java/com/livable/server/visitation/repository/ParkingLogRepository.java
@@ -2,9 +2,11 @@ package com.livable.server.visitation.repository;
 
 import com.livable.server.entity.ParkingLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ParkingLogRepository extends JpaRepository<ParkingLog, Long> {
@@ -12,4 +14,8 @@ public interface ParkingLogRepository extends JpaRepository<ParkingLog, Long> {
     @Query("select p from ParkingLog p" +
             " where p.visitor.id = :visitorId")
     Optional<ParkingLog> findParkingLogByVisitorId(@Param("visitorId") final Long visitorId);
+
+    @Modifying
+    @Query("delete from ParkingLog p where p.visitor.id in :visitorIds")
+    void deleteByVisitorIdsIn(@Param("visitorIds") List<Long> visitorIds);
 }
