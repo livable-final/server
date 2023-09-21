6c87cd44ce202b8f04d1f129d673a6212dcc7a77
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-21 17:40:55 +0900

    Refactor: 유효성 검사 테스트 리팩터링 (#55)

--- src/test/java/com/livable/server/visitation/dto/VisitationRequestTest.java
+++ src/test/java/com/livable/server/visitation/dto/VisitationRequestTest.java
@@ -3,28 +3,22 @@ package com.livable.server.visitation.dto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.livable.server.visitation.domain.VisitationValidationMessage;
-import com.livable.server.visitation.mock.MockRegisterParkingDto;
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
-
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class VisitationRequestTest {
 
@@ -43,7 +37,7 @@ class VisitationRequestTest {
     @ValueSource(strings = {"12가1234", "12흫3456", "123핡0000"})
     @ParameterizedTest(name = "[{index}] 차량번호: {0}")
     void carNumberPatternSuccessTest(String carNumber) throws JsonProcessingException {
-        VisitationRequest.RegisterParkingDto registerParkingDto = serialize(carNumber);
+        VisitationRequest.RegisterParkingDto registerParkingDto = registerParkingDtoSerialize(carNumber);
 
         Set<ConstraintViolation<VisitationRequest.RegisterParkingDto>> validate = validator.validate(registerParkingDto);
 
@@ -54,7 +48,7 @@ class VisitationRequestTest {
     @ValueSource(strings = {"123", "124", "가가가각가"})
     @ParameterizedTest(name = "[{index}] 차량번호: {0}")
     void carNumberPatternFailTest(String carNumber) throws JsonProcessingException {
-        VisitationRequest.RegisterParkingDto registerParkingDto = serialize(carNumber);
+        VisitationRequest.RegisterParkingDto registerParkingDto = registerParkingDtoSerialize(carNumber);
 
         Set<ConstraintViolation<VisitationRequest.RegisterParkingDto>> validate = validator.validate(registerParkingDto);
         List<String> errorMessages = getErrorMessages(validate);
@@ -67,7 +61,7 @@ class VisitationRequestTest {
     @ValueSource(strings = {"abcde", "124", "가가가각가"})
     @ParameterizedTest(name = "[{index}] 차량번호: {0}")
     void carNumberNotBlankSuccess(String carNumber) throws JsonProcessingException {
-        VisitationRequest.RegisterParkingDto registerParkingDto = serialize(carNumber);
+        VisitationRequest.RegisterParkingDto registerParkingDto = registerParkingDtoSerialize(carNumber);
 
         Set<ConstraintViolation<VisitationRequest.RegisterParkingDto>> validate = validator.validate(registerParkingDto);
 
@@ -81,7 +75,7 @@ class VisitationRequestTest {
     @ValueSource(strings = {"", " "})
     @ParameterizedTest(name = "[{index}] 차량번호: {0}")
     void carNumberNotBlankFailTest_1(String carNumber) throws JsonProcessingException {
-        VisitationRequest.RegisterParkingDto registerParkingDto = serialize(carNumber);
+        VisitationRequest.RegisterParkingDto registerParkingDto = registerParkingDtoSerialize(carNumber);
 
         Set<ConstraintViolation<VisitationRequest.RegisterParkingDto>> validate = validator.validate(registerParkingDto);
 
@@ -95,7 +89,7 @@ class VisitationRequestTest {
     @NullSource
     @ParameterizedTest
     void carNumberNotBlankFailTest_2(String carNumber) throws JsonProcessingException {
-        VisitationRequest.RegisterParkingDto registerParkingDto = serialize(carNumber);
+        VisitationRequest.RegisterParkingDto registerParkingDto = registerParkingDtoSerialize(carNumber);
 
         Set<ConstraintViolation<VisitationRequest.RegisterParkingDto>> validate = validator.validate(registerParkingDto);
 
@@ -105,7 +99,44 @@ class VisitationRequestTest {
         assertThat(errorMessages.contains(VisitationValidationMessage.NOT_BLANK)).isTrue();
     }
 
-    private VisitationRequest.RegisterParkingDto serialize(String carNumber) throws JsonProcessingException {
+    @DisplayName("VisitationRequest.ValidateQrCodeDto qr NotBlank 성공 테스트")
+    @Test
+    void qrNotBlankSuccess() throws JsonProcessingException {
+        VisitationRequest.ValidateQrCodeDto validateQrCodeDto = validateQrCodeDtoSerialize("123");
+
+        Set<ConstraintViolation<VisitationRequest.ValidateQrCodeDto>> validate = validator.validate(validateQrCodeDto);
+
+        List<String> errorMessages = getErrorMessages(validate);
+
+        assertThat(validate.size()).isEqualTo(0);
+    }
+
+    @DisplayName("VisitationRequest.ValidateQrCodeDto qr NotBlank 실패 테스트")
+    @NullSource
+    @ParameterizedTest
+    void qrNotBlankSuccess(String qr) throws JsonProcessingException {
+        VisitationRequest.ValidateQrCodeDto validateQrCodeDto = validateQrCodeDtoSerialize(qr);
+
+        Set<ConstraintViolation<VisitationRequest.ValidateQrCodeDto>> validate = validator.validate(validateQrCodeDto);
+
+        List<String> errorMessages = getErrorMessages(validate);
+
+        assertThat(validate.size()).isEqualTo(1);
+        assertThat(errorMessages.contains(VisitationValidationMessage.NOT_BLANK)).isTrue();
+    }
+
+    private VisitationRequest.ValidateQrCodeDto validateQrCodeDtoSerialize(String qr) throws JsonProcessingException {
+
+        if (qr == null) {
+            return new VisitationRequest.ValidateQrCodeDto();
+        }
+
+        String template = "{\"qr\":\"" + qr + "\"}";
+
+        return objectMapper.readValue(template, VisitationRequest.ValidateQrCodeDto.class);
+    }
+
+    private VisitationRequest.RegisterParkingDto registerParkingDtoSerialize(String carNumber) throws JsonProcessingException {
 
         if (carNumber == null) {
             return new VisitationRequest.RegisterParkingDto();
@@ -116,7 +147,7 @@ class VisitationRequestTest {
         return objectMapper.readValue(template, VisitationRequest.RegisterParkingDto.class);
     }
 
-    private static List<String> getErrorMessages(Set<ConstraintViolation<VisitationRequest.RegisterParkingDto>> validate) {
+    private static <T extends ConstraintViolation<?>> List<String> getErrorMessages(Set<T> validate) {
         return validate.stream()
                 .map(ConstraintViolation::getMessage)
                 .collect(Collectors.toList());
