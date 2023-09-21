4bca3d1faf25b29cc66bc68da08adc993118fcfe
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-09-22 02:47:05 +0900

    Feat: 레스토랑 리뷰 상세 데이터 응답 서비스 로직 구현 (#62)

--- src/main/java/com/livable/server/review/service/RestaurantReviewService.java
+++ src/main/java/com/livable/server/review/service/RestaurantReviewService.java
@@ -1,5 +1,9 @@
 package com.livable.server.review.service;
 
+import com.livable.server.core.exception.ErrorCode;
+import com.livable.server.core.exception.GlobalRuntimeException;
+import com.livable.server.review.domain.MyReviewErrorCode;
+import com.livable.server.review.dto.Projection;
 import com.livable.server.review.dto.RestaurantReviewResponse;
 import com.livable.server.review.repository.RestaurantReviewRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +11,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,5 +29,23 @@ public class RestaurantReviewService {
     @Transactional(readOnly = true)
     public Page<RestaurantReviewResponse.ListForMenuDTO> getAllListForMenu(Long menuId, Pageable pageable) {
         return restaurantReviewRepository.findRestaurantReviewByMenuId(menuId, pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public RestaurantReviewResponse.DetailDTO getDetail(Long reviewId) {
+
+        List<Projection.RestaurantReview> restaurantReviews
+                = restaurantReviewRepository.findRestaurantReviewById(reviewId);
+
+        if (restaurantReviews.isEmpty()) {
+            throw new GlobalRuntimeException(MyReviewErrorCode.REVIEW_NOT_EXIST);
+        }
+
+        Projection.RestaurantReview restaurantReview = restaurantReviews.get(0);
+        List<String> reviewImages = restaurantReviews.stream()
+                .map(Projection.RestaurantReview::getReviewImg)
+                .collect(Collectors.toList());
+
+        return RestaurantReviewResponse.DetailDTO.from(restaurantReview, reviewImages);
     }
 }
