ff8afd3cabfe4f0254f29bbde45f49424b7de0cf
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-21 15:37:44 +0900

    Feat: RestaurantController 주변 음식점 조회 라우터 작성 (#55)

---
+++ src/main/java/com/livable/server/restaurant/controller/RestaurantController.java
@@ -0,0 +1,35 @@
+package com.livable.server.restaurant.controller;
+
+import com.livable.server.core.response.ApiResponse;
+import com.livable.server.entity.RestaurantCategory;
+import com.livable.server.restaurant.dto.RestaurantResponse;
+import com.livable.server.restaurant.service.RestaurantService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/restaurant")
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse.Success<Object>> findRestaurantByCategory(
+            @RequestParam("type") RestaurantCategory restaurantCategory
+    ) {
+        Long visitorId = 1L;
+
+        List<RestaurantResponse.NearRestaurantDto> result =
+                restaurantService.findNearRestaurantByVisitorIdAndRestaurantCategory(visitorId, restaurantCategory);
+
+        return ApiResponse.success(result, HttpStatus.OK);
+    }
+}
