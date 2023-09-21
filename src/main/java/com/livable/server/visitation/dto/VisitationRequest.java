23040daff43c6f8d7d302dd801071dbfa4bc9b40
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-21 16:21:03 +0900

    Feat: 유효성 검사 추가 (#55)
    VisitationRequest 유효성 검사

--- src/main/java/com/livable/server/visitation/dto/VisitationRequest.java
+++ src/main/java/com/livable/server/visitation/dto/VisitationRequest.java
@@ -5,6 +5,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -12,11 +14,16 @@ public class VisitationRequest {
 
     @Getter
     public static class ValidateQrCodeDto {
+
+        @NotNull
         private String qr;
     }
 
     @Getter
     public static class RegisterParkingDto {
+
+        @NotNull
+        @NotEmpty
         @Pattern(regexp = "^\\d{2,3}[가-힣]{1}\\d{4}$", message = VisitationValidationMessage.INVALID_CAR_NUMBER)
         private String carNumber;
     }
