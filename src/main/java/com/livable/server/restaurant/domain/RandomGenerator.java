d177a2cee5591bc4a7762a399799ab9fa8c7a89c
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-09-21 16:16:41 +0900

    Refactor: RandomGenerator 리팩토링 (#55)
    공통 메서드 분리

--- src/main/java/com/livable/server/restaurant/domain/RandomGenerator.java
+++ src/main/java/com/livable/server/restaurant/domain/RandomGenerator.java
@@ -4,6 +4,14 @@ import java.util.Random;
 
 public interface RandomGenerator<T> {
 
+    default int getRandomNumber(int end) {
+        return getRandomNumber(0, end);
+    }
+
+    default int getRandomNumber(int start, int end) {
+        return new Random().nextInt(end) + start;
+    }
+
     T getRandom(int end);
     T getRandom(int start, int end);
 }
--- src/main/java/com/livable/server/restaurant/domain/RandomPageGenerator.java
+++ src/main/java/com/livable/server/restaurant/domain/RandomPageGenerator.java
@@ -4,20 +4,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
-import java.util.Random;
-
 @Component
 public class RandomPageGenerator implements RandomGenerator<Pageable> {
 
     private static final int DEFAULT_PAGE_SIZE = 5;
 
-    int getRandomNumber(int start, int end) {
-        return new Random().nextInt(end) + start;
-    }
-
     @Override
     public Pageable getRandom(int end) {
-        return getRandom(1, end);
+        return getRandom(0, end);
     }
 
     @Override
