1615ed9154d9ed1d523720bbd04a86a98bbf8d85
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-09-22 02:45:44 +0900

    Feat: 레스토랑 리뷰 상세 데이터 응답 레포지토리 구현 (#62)

--- src/main/java/com/livable/server/review/repository/RestaurantReviewCustomRepository.java
+++ src/main/java/com/livable/server/review/repository/RestaurantReviewCustomRepository.java
@@ -1,12 +1,17 @@
 package com.livable.server.review.repository;
 
+import com.livable.server.review.dto.Projection;
 import com.livable.server.review.dto.RestaurantReviewResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface RestaurantReviewCustomRepository {
 
     Page<RestaurantReviewResponse.ListDTO> findRestaurantReviewByBuildingId(Long buildingId, Pageable pageable);
 
     Page<RestaurantReviewResponse.ListForMenuDTO> findRestaurantReviewByMenuId(Long menuId, Pageable pageable);
+
+    List<Projection.RestaurantReview> findRestaurantReviewById(Long reviewId);
 }
