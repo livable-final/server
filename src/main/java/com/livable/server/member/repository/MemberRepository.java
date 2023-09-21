34f6944ba76917b686462b227a101e93da8ddc36
jongy <jongyoon+u@gmail.com>
2023-09-22 05:55:56 +0900

    Feat : MemberRepository 구현 (#42)
    사용자 Id를 통해 빌딩 정보 조회

--- src/main/java/com/livable/server/member/repository/MemberRepository.java
+++ src/main/java/com/livable/server/member/repository/MemberRepository.java
@@ -1,7 +1,20 @@
 package com.livable.server.member.repository;
 
 import com.livable.server.entity.Member;
+import com.livable.server.home.dto.HomeResponse.BuildingInfoDto;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	@Query(value = "select b.id as buildingId, b.name as buildingName, b.hasCafeteria as hasCafeteria" +
+			" from Member m " +
+			" join Company c" +
+			" on m.company = c" +
+			" join fetch Building b " +
+			" on c.building = b" +
+			" where m.id = :memberId")
+	Optional<BuildingInfoDto> findBuildingInfoByMemberId(@Param("memberId") Long memberId);
 }
