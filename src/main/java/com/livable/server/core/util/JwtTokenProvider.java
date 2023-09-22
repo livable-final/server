3ec8a479e581a05d49a1e2b513189dfff65d3ff3
khsrla9806 <kimhunsope@naver.com>
2023-09-22 21:10:42 +0900

    Feat: Jwt 기능을 담당하는 컴포넌트, Enum 생성 (#76)

---
+++ src/main/java/com/livable/server/core/util/JwtTokenProvider.java
@@ -0,0 +1,53 @@
+package com.livable.server.core.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    /**
+     * Actor 정보를 입력하여 JWT 토큰을 생성하는 메서드
+     * @param actorType: 엑터의 종류
+     * @param actorId: 엑터의 식별값
+     * @param expireDate: 토큰의 만료일
+     * @return 입력된 정보로 만든 토큰을 반환
+     */
+    public String createActorToken(ActorType actorType, Long actorId, Date expireDate) {
+
+        Claims claims = Jwts.claims();
+        claims.put("actorId", actorId);
+        claims.put("actorType", actorType);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    /**
+     * 토큰의 유효성을 검사하는 메서드
+     * @param token: JWT 토큰 입력
+     * @return 입력한 토큰이 유효하면 true, 유효하지 않으면 false
+     */
+    public boolean isValidateToken(String token) {
+        try {
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+
+            return claimsJws.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
