3b418dcefd7792b17ff3ae2a606b9558dd21f375
jongy <jongyoon+u@gmail.com>
2023-09-22 19:18:48 +0900

    Feat : 출입 카드 확인 Controller 코드 작성 (#69)
    누락 코드 추가

--- src/main/java/com/livable/server/home/controller/HomeController.java
+++ src/main/java/com/livable/server/home/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.livable.server.home.controller;
 
 import com.livable.server.core.response.ApiResponse;
 import com.livable.server.home.dto.HomeResponse.AccessCardDto;
+import com.livable.server.home.dto.HomeResponse.BuildingInfoDto;
 import com.livable.server.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	private final MemberService memberService;
+
+	@GetMapping("/api/home")
+	public ResponseEntity<ApiResponse.Success<BuildingInfoDto>> getHomeInfo() {
+
+		Long memberId = 1L; // TODO: 2023-09-22 JWT Token으로 대체
+		BuildingInfoDto buildingInfoDto = memberService.getBuildingInfo(memberId);
+
+		return ApiResponse.success(buildingInfoDto, HttpStatus.OK);
+	}
 
 	@GetMapping("/api/access-card")
 	public ResponseEntity<ApiResponse.Success<AccessCardDto>> getAccessCard(){
