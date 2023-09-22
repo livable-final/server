a611d5ac57a600a9b4ba5debe6610c19ffc97068
khsrla9806 <kimhunsope@naver.com>
2023-09-22 18:59:12 +0900

    Feat: 클라이언트 통신을 위한 Cors 설정 (#51)

--- src/main/java/com/livable/server/core/config/WebConfig.java
+++ src/main/java/com/livable/server/core/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.livable.server.core.config;
 import com.livable.server.core.util.StringToRestaurantCategoryConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToRestaurantCategoryConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .exposedHeaders("Authorization")
+                .allowedOriginPatterns("*");
     }
 }
