39986bfa9609b0761bb0e49eb688f3e6a405d945
jongy <jongyoon+u@gmail.com>
2023-09-22 19:11:56 +0900

    Feat : 출입 카드 확인 Service 코드 작성 (#69)

--- src/main/java/com/livable/server/member/service/MemberService.java
+++ src/main/java/com/livable/server/member/service/MemberService.java
@@ -3,12 +3,13 @@ package com.livable.server.member.service;
 import com.livable.server.core.exception.GlobalRuntimeException;
 import com.livable.server.home.dto.HomeResponse;
 import com.livable.server.member.domain.MemberErrorCode;
+import com.livable.server.member.dto.AccessCardProjection;
 import com.livable.server.member.dto.MemberResponse;
 import com.livable.server.member.dto.MyPageProjection;
 import com.livable.server.member.repository.MemberRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -24,10 +25,19 @@ public class MemberService {
 
         return MemberResponse.MyPageDTO.from(myPageProjection);
     }
-    
-    public HomeResponse.BuildingInfoDto getBuildingInfo(Long memberId) {
-		  return memberRepository.findBuildingInfoByMemberId(memberId)
-			  	.orElseThrow(() -> new GlobalRuntimeException(MemberErrorCode.BUILDING_INFO_NOT_EXIST));
+
+    public HomeResponse.AccessCardDto getAccessCardData(Long memberId) {
+
+        Optional<AccessCardProjection> accessCardProjectionOptional = memberRepository.findAccessCardData(memberId);
+        AccessCardProjection accessCardProjection = accessCardProjectionOptional.orElseThrow(()
+            -> new GlobalRuntimeException(MemberErrorCode.RETRIEVE_ACCESSCARD_FAILED));
+
+        return HomeResponse.AccessCardDto.from(accessCardProjection);
     }
-  
+
+    public HomeResponse.BuildingInfoDto getBuildingInfo(Long memberId) {
+		  return memberRepository.findBuildingInfoByMemberId(memberId).orElseThrow(()
+          -> new GlobalRuntimeException(MemberErrorCode.BUILDING_INFO_NOT_EXIST));
+    }
+
 }
