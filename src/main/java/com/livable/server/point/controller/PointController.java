21aaf90a8e2e08ad3778174ae403e82b1e4b1c2e
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-09-24 22:42:45 +0900

    Feat: 포인트 목표 달성 데이터 응답 엔드 포인트 구현 (#75)

---
+++ src/main/java/com/livable/server/point/controller/PointController.java
@@ -0,0 +1,29 @@
+package com.livable.server.point.controller;
+
+import com.livable.server.core.response.ApiResponse;
+import com.livable.server.point.dto.PointResponse;
+import com.livable.server.point.service.PointService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController
+public class PointController {
+
+    private final PointService pointService;
+
+    @GetMapping("/api/points/logs/members")
+    public ResponseEntity<ApiResponse.Success<PointResponse.ReviewCountDTO>> getMyReviewCount() {
+
+        Long memberId = 1L; // TODO: 토큰에서 값을 추출할 것
+        LocalDateTime currentDate = LocalDateTime.now();
+
+        PointResponse.ReviewCountDTO myReviewCount = pointService.getMyReviewCount(memberId, currentDate);
+        return ApiResponse.success(myReviewCount, HttpStatus.OK);
+    }
+}
