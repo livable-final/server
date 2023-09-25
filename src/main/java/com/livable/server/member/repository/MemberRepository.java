a91fd0338db4b609598b76196069a6f201d7a163
jongy <jongyoon+u@gmail.com>
2023-09-25 13:58:44 +0900

    Fix : 홈 화면 정보 쿼리 수정 (#42)
    매핑 class 추가로 인한 변경 사항 적용
    Convention 통일

--- src/main/java/com/livable/server/member/repository/MemberRepository.java
+++ src/main/java/com/livable/server/member/repository/MemberRepository.java
@@ -1,10 +1,9 @@
 package com.livable.server.member.repository;
 
 import com.livable.server.entity.Member;
-import com.livable.server.home.dto.HomeResponse.BuildingInfoDto;
-import java.util.Optional;
-
+import com.livable.server.member.dto.BuildingInfoProjection;
 import com.livable.server.member.dto.MyPageProjection;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,13 +17,13 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
             "WHERE m.id = :memberId")
     Optional<MyPageProjection> findMemberCompanyPointData(@Param("memberId") Long memberId);
 
-    @Query("select b.id as buildingId, b.name as buildingName, b.hasCafeteria as hasCafeteria" +
-            " from Member m " +
-            " join Company c" +
-            " on m.company = c" +
-            " join fetch Building b " +
-            " on c.building = b" +
-            " where m.id = :memberId")
-	  Optional<BuildingInfoDto> findBuildingInfoByMemberId(@Param("memberId") Long memberId);
+    @Query("SELECT new com.livable.server.member.dto.BuildingInfoProjection (b.id, b.name, b.hasCafeteria) " +
+            "FROM Member m " +
+            "JOIN Company c " +
+            "ON m.company = c " +
+            "JOIN Building b " +
+            "ON c.building = b " +
+            "WHERE m.id = :memberId")
+	  Optional<BuildingInfoProjection> findBuildingInfoByMemberId(@Param("memberId") Long memberId);
   
 }
