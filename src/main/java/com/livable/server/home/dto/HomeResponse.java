6d617600ec3788c0bd624e9c1cf6d13cc36e8ab9
jongy <jongyoon+u@gmail.com>
2023-09-25 13:57:40 +0900

    Fix : 홈 화면 정보 응답 DTO 수정 (#42)
    builder 추가

--- src/main/java/com/livable/server/home/dto/HomeResponse.java
+++ src/main/java/com/livable/server/home/dto/HomeResponse.java
@@ -1,20 +1,29 @@
 package com.livable.server.home.dto;
 
+import com.livable.server.member.dto.BuildingInfoProjection;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class HomeResponse {
 
-	@Getter
-	@AllArgsConstructor
-	public static class BuildingInfoDto {
+		@Getter
+		@Builder
+		public static class BuildingInfoDto {
 
-		private Long buildingId;
-		private String buildingName;
-		private Boolean hasCafeteria;
-	}
+			private Long buildingId;
+			private String buildingName;
+			private Boolean hasCafeteria;
+
+			public static BuildingInfoDto from(BuildingInfoProjection buildingInfoProjection) {
+				return BuildingInfoDto.builder()
+						.buildingId(buildingInfoProjection.getBuildingId())
+						.buildingName(buildingInfoProjection.getBuildingName())
+						.hasCafeteria(buildingInfoProjection.getHasCafeteria())
+						.build();
+			}
+		}
 
 }
