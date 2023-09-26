8afd6682ccba60de55a17d4ca712cb56bf64489a
edan_cafe <roseswallow13@gamil.com>
2023-09-26 20:47:21 +0900

    Feat: 리뷰 ENUM 에러 코드 타입 구현 (#50)

---
+++ src/main/java/com/livable/server/review/domain/ReviewErrorCode.java
@@ -0,0 +1,29 @@
+package com.livable.server.review.domain;
+
+import com.livable.server.core.exception.ErrorCode;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ReviewErrorCode implements ErrorCode {
+    MEMBER_NOT_EXIST(HttpStatus.BAD_REQUEST, "존재하지 않는 회원 정보입니다."),
+    RESTAURANT_NOT_EXITST(HttpStatus.BAD_REQUEST, "존재하지 않는 음식점 정보입니다."),
+    MENUS_NOT_CHOICE(HttpStatus.BAD_REQUEST, "하나 이상의 메뉴를 선택해 주세요.");
+
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
