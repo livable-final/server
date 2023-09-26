d65a4a6322d7e8ac040b76535e63eb92612b0967
edan_cafe <roseswallow13@gamil.com>
2023-09-26 20:47:53 +0900

    Feat: 리뷰 이미지 레포지토리 생성 (#50)

---
+++ src/main/java/com/livable/server/review/repository/ReviewImageRepository.java
@@ -0,0 +1,7 @@
+package com.livable.server.review.repository;
+
+import com.livable.server.entity.ReviewImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> {
+}
