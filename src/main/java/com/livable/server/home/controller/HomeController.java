b2bd29acbbf73e54290c4cc43da43aa19daa0250
jongy <jongyoon+u@gmail.com>
2023-09-26 11:05:49 +0900

    Refactor : JWT 변경 사항 적용 (#69)
    JWT 토큰 추가로 인한 코드 변경 사항 적용

--- src/main/java/com/livable/server/home/controller/HomeController.java
+++ src/main/java/com/livable/server/home/controller/HomeController.java
@@ -4,23 +4,22 @@ import com.livable.server.core.response.ApiResponse;
 import com.livable.server.core.util.Actor;
 import com.livable.server.core.util.JwtTokenProvider;
 import com.livable.server.core.util.LoginActor;
+import com.livable.server.home.dto.HomeResponse.AccessCardDto;
 import com.livable.server.home.dto.HomeResponse.BuildingInfoDto;
 import com.livable.server.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/home")
 @RestController
 public class HomeController {
 
 	private final MemberService memberService;
 
-	@GetMapping
+	@GetMapping("api/home")
 	public ResponseEntity<ApiResponse.Success<BuildingInfoDto>> getHomeInfo(@LoginActor Actor actor) {
 
 		JwtTokenProvider.checkMemberToken(actor);
@@ -32,9 +31,11 @@ public class HomeController {
 	}
 
 	@GetMapping("/api/access-card")
-	public ResponseEntity<ApiResponse.Success<AccessCardDto>> getAccessCard(){
+	public ResponseEntity<ApiResponse.Success<AccessCardDto>> getAccessCard(@LoginActor Actor actor){
 
-		Long memberId = 1L; // TODO: 2023-09-22 JWT Token으로 대체
+		JwtTokenProvider.checkMemberToken(actor);
+
+		Long memberId = actor.getId();
 
 		AccessCardDto accessCardDto = memberService.getAccessCardData(memberId);
 
