d826b40cae7a9bb69cc5b1f09da73daaa8718529
JY-B <61049995+jy-b@users.noreply.github.com>
2023-09-26 12:45:19 +0900

    회원 출입카드 정보 응답 구현 (#87)
    * Feat : 출입 카드 확인 Controller 구현 (#69)
    
    출입 카드 확인 관련 코드 구현
    
    * Feat : 출입 카드 확인 용 클래스 추가 (#69)
    
    * Test : 출입 카드 확인 Controller 테스트 코드 작성 (#69)
    
    * Feat : 출입 카드 확인 Repository 코드 작성 (#69)
    
    * Feat : 출입 카드 확인 Service 코드 작성 (#69)
    
    * Feat : 출입 카드 확인 Controller 코드 작성 (#69)
    
    누락 코드 추가
    
    * Test : 출입 카드 확인 Service 테스트 코드 작성 (#69)
    
    * Docs : 코드 스타일 수정 (#69)
    
    * Typo : 오타 수정 (#69)
    
    * Feat : 출입 카드 확인 Repository 코드 수정 (#69)
    
    Select 결과가 여러개일 경우 가장 첫번째 결과값을 매핑하도록 수정
    
    * Feat : 출입 카드 확인 Service 코드 수정 (#69)
    
    쿼리 수정으로 인한 변경 사항 반영
    
    * TEST : 출입 카드 확인 Service 테스트 코드 수정 (#69)
    
    쿼리 수정으로 인한 변경 사항 반영
    
    * Refactor : 룰렛 카테고리, 메뉴 응답용 Controller 변경 사항 적용 (#90)
    
    JWT 토큰 추가로 인한 코드 변경 사항 적용
    
    * Refactor : JWT 변경 사항 적용 (#69)
    
    JWT 토큰 추가로 인한 코드 변경 사항 적용

--- src/main/java/com/livable/server/member/repository/MemberRepository.java
+++ src/main/java/com/livable/server/member/repository/MemberRepository.java
@@ -2,9 +2,10 @@ package com.livable.server.member.repository;
 
 import com.livable.server.entity.Member;
 import com.livable.server.home.dto.HomeResponse.BuildingInfoDto;
-import java.util.Optional;
-
+import com.livable.server.member.dto.AccessCardProjection;
 import com.livable.server.member.dto.MyPageProjection;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,13 +19,21 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
             "WHERE m.id = :memberId")
     Optional<MyPageProjection> findMemberCompanyPointData(@Param("memberId") Long memberId);
 
-    @Query("select b.id as buildingId, b.name as buildingName, b.hasCafeteria as hasCafeteria" +
-            " from Member m " +
-            " join Company c" +
-            " on m.company = c" +
-            " join fetch Building b " +
-            " on c.building = b" +
-            " where m.id = :memberId")
+    @Query("SELECT distinct new com.livable.server.member.dto.AccessCardProjection(b.name, m.employeeNumber, c.name, o.floor, o.roomNumber, m.name) " +
+            "FROM Member m " +
+            "INNER JOIN Company c ON c.id = m.company.id " +
+            "INNER JOIN Office o ON c.id = o.company.id " +
+            "INNER JOIN Building b ON b.id = c.building.id " +
+            "WHERE m.id = :memberId")
+    List<AccessCardProjection> findAccessCardData(@Param("memberId") Long memberId);
+
+    @Query("SELECT b.id AS buildingId, b.name AS buildingName, b.hasCafeteria AS hasCafeteria " +
+            "FROM Member m " +
+            "JOIN Company c " +
+            "ON m.company = c " +
+            "JOIN FETCH Building b " +
+            "ON c.building = b " +
+            "WHERE m.id = :memberId")
 	  Optional<BuildingInfoDto> findBuildingInfoByMemberId(@Param("memberId") Long memberId);
-  
+
 }
