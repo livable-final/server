43b1cacb0e319fd5b6788e70cded54957e973189
jongy <jongyoon+u@gmail.com>
2023-09-27 17:46:35 +0900

    Test : 메뉴를 판매 하고 있는 식당 목록 응답 Controller 테스트 작성 (#100)

--- src/test/java/com/livable/server/restaurant/controller/RestaurantControllerTest.java
+++ src/test/java/com/livable/server/restaurant/controller/RestaurantControllerTest.java
@@ -1,13 +1,28 @@
 package com.livable.server.restaurant.controller;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.livable.server.core.exception.GlobalRuntimeException;
 import com.livable.server.core.util.ActorType;
 import com.livable.server.core.util.JwtTokenProvider;
 import com.livable.server.core.util.TestConfig;
 import com.livable.server.entity.RestaurantCategory;
+import com.livable.server.restaurant.domain.RestaurantErrorCode;
 import com.livable.server.restaurant.dto.RestaurantResponse;
+import com.livable.server.restaurant.dto.RestaurantResponse.RestaurantsByMenuDto;
 import com.livable.server.restaurant.mock.MockNearRestaurantDto;
+import com.livable.server.restaurant.mock.MockRestaurantByMenuDto;
 import com.livable.server.restaurant.service.RestaurantService;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +31,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import static org.mockito.BDDMockito.given;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @Import(TestConfig.class)
 @WebMvcTest(RestaurantController.class)
@@ -67,5 +73,52 @@ class RestaurantControllerTest {
         // then
         resultActions.andExpect(status().isOk())
                 .andExpect(jsonPath("$.data").isArray());
+    }
+
+    @DisplayName("SUCCESS - 메뉴별 식당 목록 응답 컨트롤러 테스트")
+    @Test
+    void findRestaurantByMenuSuccess() throws Exception {
+
+        // given
+        String token = tokenProvider.createActorToken(ActorType.MEMBER, 1L, new Date(new Date().getTime() + 10000000));
+
+        List<RestaurantsByMenuDto> result =
+            IntStream.range(1, 10)
+                .mapToObj(idx -> new MockRestaurantByMenuDto())
+                .collect(Collectors.toList());
+
+        given(restaurantService.findRestaurantByMenuId(anyLong(), anyLong()))
+            .willReturn(result);
+
+        // when & then
+        mockMvc.perform(get("/api/restaurants")
+            .header("Authorization", "Bearer " + token)
+            .queryParam("menuId", "1")
+        )
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data").isArray());
+
+    }
+
+    @DisplayName("FAILED - 메뉴별 식당 목록 응답 컨트롤러 테스트")
+    @Test
+    void findRestaurantByMenuFailed() throws Exception {
+
+        //given
+        String token = tokenProvider.createActorToken(ActorType.MEMBER, 1L,
+            new Date(new Date().getTime() + 10000000));
+
+        given(restaurantService.findRestaurantByMenuId(anyLong(), anyLong()))
+            .willThrow(
+                new GlobalRuntimeException(RestaurantErrorCode.NOT_FOUND_RESTAURANT_BY_MENU));
+
+        // when & then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/restaurants")
+                .header("Authorization", "Bearer " + token)
+                .queryParam("menuId", "1")
+            )
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value(
+                RestaurantErrorCode.NOT_FOUND_RESTAURANT_BY_MENU.getMessage()));
     }
 }
