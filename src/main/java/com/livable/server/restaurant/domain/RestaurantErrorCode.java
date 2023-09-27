adedc662407fca53844f4909f85735d599314346
jongy <jongyoon+u@gmail.com>
2023-09-27 17:41:19 +0900

    Feat : 메뉴를 판매 하고 있는 식당 목록 응답용 DTO 추가 (#100)

--- src/main/java/com/livable/server/restaurant/domain/RestaurantErrorCode.java
+++ src/main/java/com/livable/server/restaurant/domain/RestaurantErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum RestaurantErrorCode implements ErrorCode {
-    NOT_FOUND_CATEGORY(HttpStatus.BAD_REQUEST, "존재하지 않는 식당 종류입니다.");
+    NOT_FOUND_CATEGORY(HttpStatus.BAD_REQUEST, "존재하지 않는 식당 종류입니다."),
+
+    NOT_FOUND_RESTAURANT_BY_MENU(HttpStatus.BAD_REQUEST, "해당 메뉴를 제공하는 식당을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
