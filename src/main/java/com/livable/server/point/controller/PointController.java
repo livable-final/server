c10395933d8d4bde7a360bf1bdabf0688a71c4fa
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-09-27 19:15:40 +0900

    Feat: 목표 달성 포인트 지급 엔드포인트 구현  (#106)

--- src/main/java/com/livable/server/point/controller/PointController.java
+++ src/main/java/com/livable/server/point/controller/PointController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -25,5 +26,15 @@ public class PointController {
 
         PointResponse.ReviewCountDTO myReviewCount = pointService.getMyReviewCount(memberId, currentDate);
         return ApiResponse.success(myReviewCount, HttpStatus.OK);
+    }
+
+    @PostMapping("/api/points/logs/members")
+    public ResponseEntity<ApiResponse.Success<Object>> getAchievementPoint() {
+
+        Long memberId = 1L; // TODO: 토큰에서 값을 추출할 것
+        LocalDateTime requestDateTime = LocalDateTime.now();
+
+        pointService.getAchievementPoint(memberId, requestDateTime);
+        return ApiResponse.success(HttpStatus.CREATED);
     }
 }
