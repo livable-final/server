dd04c50addbc91267860c74d186f7b660a4a8cb3
jongy <jongyoon+u@gmail.com>
2023-09-28 19:51:05 +0900

    Feat : 가장 많이 선택한 메뉴 10위까지 응답용 Repository 구현 (#99)

--- src/main/java/com/livable/server/menu/repository/MenuRepository.java
+++ src/main/java/com/livable/server/menu/repository/MenuRepository.java
@@ -1,10 +1,13 @@
 package com.livable.server.menu.repository;
 
 import com.livable.server.entity.Menu;
+import com.livable.server.menu.dto.MostSelectedMenuProjection;
 import com.livable.server.menu.dto.RouletteMenuProjection;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -17,4 +20,14 @@ public interface MenuRepository extends JpaRepository<Menu, Long> {
     )
     List<RouletteMenuProjection> findRouletteMenus();
 
+    @Query(
+        "SELECT new com.livable.server.menu.dto.MostSelectedMenuProjection(mcr.count, mcr.date, mcr.menu.id, m.name,  m.representativeImageUrl) " +
+        "FROM MenuChoiceResult mcr " +
+        "JOIN Menu m " +
+        "ON m.id = mcr.menu.id " +
+        "WHERE mcr.building.id = :buildingId AND mcr.date = CURRENT_DATE " +
+        "GROUP BY mcr.date, mcr.menu.id, m.name, m.representativeImageUrl " +
+        "ORDER BY mcr.count DESC "
+    )
+    List<MostSelectedMenuProjection> findMostSelectedMenuOrderByCount(@Param("buildingId") Long buildingId, Pageable pageable);
 }
