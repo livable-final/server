1c855b6cb7bcccabff22939b2fab05eb534b19f5
jongy <jongyoon+u@gmail.com>
2023-09-28 19:52:53 +0900

    Feat : 가장 많이 선택한 메뉴 10위까지 응답용 Controller 구현 (#99)

--- src/main/java/com/livable/server/menu/controller/MenuController.java
+++ src/main/java/com/livable/server/menu/controller/MenuController.java
@@ -1,10 +1,13 @@
 package com.livable.server.menu.controller;
 
+import static com.livable.server.menu.domain.MenuPaging.MOST_SELECTED_MENU;
+
 import com.livable.server.core.response.ApiResponse;
 import com.livable.server.core.response.ApiResponse.Success;
 import com.livable.server.core.util.Actor;
 import com.livable.server.core.util.JwtTokenProvider;
 import com.livable.server.core.util.LoginActor;
+import com.livable.server.menu.dto.MenuResponse.MostSelectedMenuDTO;
 import com.livable.server.menu.dto.MenuResponse.RouletteMenuDTO;
 import com.livable.server.menu.service.MenuService;
 import java.util.List;
@@ -12,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -28,6 +32,16 @@ public class MenuController {
         List<RouletteMenuDTO> rouletteMenuDTOs = menuService.getRouletteMenus();
 
         return ApiResponse.success(rouletteMenuDTOs, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/menus/buildings/{buildingId}")
+    public ResponseEntity<Success<List<MostSelectedMenuDTO>>> getMostSelectedMenu(@PathVariable("buildingId") Long buildingId, @LoginActor Actor actor) {
+
+        JwtTokenProvider.checkMemberToken(actor);
+
+        List<MostSelectedMenuDTO> mostSelectedMenu = menuService.getMostSelectedMenu(buildingId, MOST_SELECTED_MENU.getLimit());
+
+        return ApiResponse.success(mostSelectedMenu, HttpStatus.OK);
     }
 
 }
