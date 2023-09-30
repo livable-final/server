9755e343b6aab76dbfdb4cdd0301462946fd006c
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-09-30 21:44:19 +0900

    feat: 특정 Point의 월별 단위 PointLog를 검색하는 쿼리 추가 (#145)

--- src/main/java/com/livable/server/point/repository/PointLogRepository.java
+++ src/main/java/com/livable/server/point/repository/PointLogRepository.java
@@ -5,11 +5,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PointLogRepository extends JpaRepository<PointLog, Long> {
 
-    @Query(value = "SELECT * FROM point_log WHERE DATE(created_at) = :date", nativeQuery = true)
-    List<PointLog> findLogsByDate(@Param("date") LocalDate date);
+    @Query(value = "SELECT * " +
+            "FROM point_log " +
+            "WHERE point_id = :pointId " +
+            "AND created_at BETWEEN :startDate AND :endDate " +
+            "ORDER BY created_at DESC", nativeQuery = true)
+    List<PointLog> findDateRangeOfPointLogByPointId(
+            @Param("pointId") Long pointId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate
+    );
 }
