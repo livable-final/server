e62753c15051c0c8df55a8fbdc6ae39e27e66fdc
edan_cafe <roseswallow13@gamil.com>
2023-09-30 16:35:14 +0900

    Add: 음식점에서 판매하는 메뉴 응답 DTO 기능구현 (#116)

--- src/main/java/com/livable/server/restaurant/dto/RestaurantResponse.java
+++ src/main/java/com/livable/server/restaurant/dto/RestaurantResponse.java
@@ -1,10 +1,8 @@
 package com.livable.server.restaurant.dto;
 
+import com.livable.server.entity.Menu;
 import com.livable.server.entity.RestaurantCategory;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RestaurantResponse {
@@ -24,5 +22,12 @@ public class RestaurantResponse {
         private Integer floor;
 
         private String url;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class listMenuDTO {
+        private Long menuId;
+        private String menuName;
     }
 }
