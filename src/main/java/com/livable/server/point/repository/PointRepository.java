f2fcc414f8c649e124bdfb6aa2f3ffa57646a8fe
정현수 <junghss0113@gmail.com>
2023-09-30 21:51:31 +0900

    목표 달성 포인트 획득 기능 개선 완료 (#159)
    * Refactor: PointLog 엔티티에 LocalDate를 받아 해당 날짜에 작성되었는지를 반환하는 기능 추가 (#145)
    
    * feat: 특정 Point의 월별 단위 PointLog를 검색하는 쿼리 추가 (#145)
    
    * Modify: 사용되지 않는 쿼리 삭제 (#145)
    
    * Modify: 쿼리 메서드 삭제로 인해 사용되지 않는 매핑 객체 삭제 (#145)
    
    * Refactor: 목표 달성 포인트 지급 서비스 로직 성능 개선 (#145)
    기존 4번의 IO가 발생하던 로직을 2번의 IO가 발생하도록 기능 개선
    
    * Test: 테스트 베이스 코드 작성 (#145)

--- src/main/java/com/livable/server/point/repository/PointRepository.java
+++ src/main/java/com/livable/server/point/repository/PointRepository.java
@@ -2,7 +2,6 @@ package com.livable.server.point.repository;
 
 import com.livable.server.entity.Point;
 import com.livable.server.entity.PointCode;
-import com.livable.server.point.dto.PointProjection;
 import com.livable.server.point.dto.PointResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -22,29 +21,6 @@ public interface PointRepository extends JpaRepository<Point, Long> {
             "AND pl.createdAt BETWEEN :startDate AND :endDate " +
             "AND pl.code IN (:codes)")
     PointResponse.ReviewCountDTO findPointCountById(
-            @Param("pointId") Long pointId,
-            @Param("startDate") LocalDateTime startDate,
-            @Param("endDate") LocalDateTime endDate,
-            @Param("codes") List<PointCode> codes);
-
-    @Query("SELECT new com.livable.server.point.dto.PointProjection$CountAndDateDTO(COUNT(pl.id), MAX(pl.createdAt)) " +
-            "FROM PointLog pl " +
-            "WHERE pl.point.id = :pointId " +
-            "AND pl.createdAt BETWEEN :startDate AND :endDate " +
-            "AND pl.code IN (:codes)")
-    PointProjection.CountAndDateDTO findCountAndDateById(
-            @Param("pointId") Long pointId,
-            @Param("startDate") LocalDateTime startDate,
-            @Param("endDate") LocalDateTime endDate,
-            @Param("codes") List<PointCode> codes);
-
-    @Query("SELECT new com.livable.server.point.dto.PointProjection$ReviewAndDateDTO(pl.review, pl.createdAt) " +
-            "FROM PointLog pl " +
-            "WHERE pl.point.id = :pointId " +
-            "AND pl.createdAt BETWEEN :startDate AND :endDate " +
-            "AND pl.code IN (:codes)" +
-            "ORDER BY pl.createdAt DESC")
-    List<PointProjection.ReviewAndDateDTO> findReviewAndDateById(
             @Param("pointId") Long pointId,
             @Param("startDate") LocalDateTime startDate,
             @Param("endDate") LocalDateTime endDate,
--- src/test/java/com/livable/server/point/service/PointServiceTest.java
+++ src/test/java/com/livable/server/point/service/PointServiceTest.java
@@ -2,11 +2,17 @@ package com.livable.server.point.service;
 
 import com.livable.server.core.exception.GlobalRuntimeException;
 import com.livable.server.entity.Point;
+import com.livable.server.entity.PointCode;
+import com.livable.server.entity.PointLog;
+import com.livable.server.entity.Review;
 import com.livable.server.point.domain.DateFactory;
 import com.livable.server.point.domain.DateRange;
 import com.livable.server.point.dto.PointResponse;
 import com.livable.server.point.repository.PointRepository;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
@@ -15,6 +21,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
@@ -80,6 +87,52 @@ class PointServiceTest {
             // Then
             Assertions.assertThrows(GlobalRuntimeException.class, () ->
                     pointService.getMyReviewCount(memberId, currentDate));
+        }
+    }
+
+    @Nested
+    @DisplayName("목표 달성 포인트 지급 서비스 단위 테스트")
+    class GetAchievementPoint {
+
+        @DisplayName("성공 - 일곱개의 리뷰 포인트 로그가 주어지는 경우, 목표 포인트를 지급한다.")
+        @Test
+        void success_Test_GivenSevenReviewPointLog_ThenPaidAchievementPoint() {
+            // Given
+            Point point = Point.builder().balance(10).build();
+            Review review = Review.builder().id(1L).build();
+            List<PointLog> MockList = List.of(
+                    PointLog.builder().id(1L).point(point).review(review).code(PointCode.PA00).build(),
+                    PointLog.builder().id(2L).point(point).review(review).code(PointCode.PA01).build(),
+                    PointLog.builder().id(3L).point(point).review(review).code(PointCode.PA00).build(),
+                    PointLog.builder().id(4L).point(point).review(review).code(PointCode.PA00).build(),
+                    PointLog.builder().id(5L).point(point).review(review).code(PointCode.PA02).build(),
+                    PointLog.builder().id(6L).point(point).review(review).code(PointCode.PA00).build(),
+                    PointLog.builder().id(7L).point(point).review(review).code(PointCode.PA01).build()
+            );
+            // When
+
+            // Then
+        }
+
+        @DisplayName("실패 - 목표는 달성 했지만 포인트 신청 날짜가 목표 달성 당일이 아닌 경우, 예외를 발생한다.")
+        @Test
+        void failure_Test_GivenRequestNotAchievingDate_ThenThrowError() {
+            // Given
+            Point point = Point.builder().balance(10).build();
+            Review review = Review.builder().id(1L).build();
+            List<PointLog> MockList = List.of(
+                    PointLog.builder().id(1L).point(point).review(review).code(PointCode.PA00).build(),
+                    PointLog.builder().id(2L).point(point).review(review).code(PointCode.PA01).build(),
+                    PointLog.builder().id(3L).point(point).review(review).code(PointCode.PA00).build(),
+                    PointLog.builder().id(4L).point(point).review(review).code(PointCode.PA00).build(),
+                    PointLog.builder().id(5L).point(point).review(review).code(PointCode.PA02).build(),
+                    PointLog.builder().id(6L).point(point).review(review).code(PointCode.PA00).build(),
+                    PointLog.builder().id(7L).point(point).review(review).code(PointCode.PA01).build()
+            );
+
+            // When
+
+            // Then
         }
     }
 }
