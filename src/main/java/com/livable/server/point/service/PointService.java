fd2f8eb91f11c43d99a74876d7d0499df4bd8d58
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-09-30 21:46:33 +0900

    Refactor: 목표 달성 포인트 지급 서비스 로직 성능 개선 (#145) 기존 4번의 IO가 발생하던 로직을 2번의 IO가 발생하도록 기능 개선

--- src/main/java/com/livable/server/point/service/PointService.java
+++ src/main/java/com/livable/server/point/service/PointService.java
@@ -9,7 +9,6 @@ import com.livable.server.point.domain.DateFactory;
 import com.livable.server.point.domain.DateRange;
 import com.livable.server.point.domain.PointAchievement;
 import com.livable.server.point.domain.PointErrorCode;
-import com.livable.server.point.dto.PointProjection;
 import com.livable.server.point.dto.PointResponse;
 import com.livable.server.point.repository.PointLogRepository;
 import com.livable.server.point.repository.PointRepository;
@@ -51,65 +50,110 @@ public class PointService {
     @Transactional
     public void getAchievementPoint(Long memberId, LocalDateTime requestDateTime) {
 
-        System.out.println("requestDateTime:" + requestDateTime);
-
         // 회원 정보가 유효한지 검증
-        Point point = pointRepository.findByMemberId(memberId).orElseThrow(() ->
+        final Point point = pointRepository.findByMemberId(memberId).orElseThrow(() ->
                 new GlobalRuntimeException(PointErrorCode.POINT_NOT_EXIST));
 
-        // 금일 이미 목표 달성 포인트를 지급받았는지 확인
+        // 회원의 요청 날짜에 대한 한달 범위의 포인트 로그를 검색
+        final List<PointLog> pointLogs = this.getPointLogPerMonthBy(requestDateTime, point);
+        PointLog recentPointLog = this.getRecentPointLogFrom(pointLogs);
         LocalDate requestDate = dateFactory.getPureDate(requestDateTime);
-        List<PointLog> logsByDate = pointLogRepository.findLogsByDate(requestDate);
-        logsByDate.forEach(pointLog -> {
-            if (PointAchievement.POINT_CODES.contains(pointLog.getCode())) {
-                throw new GlobalRuntimeException(PointErrorCode.ACHIEVEMENT_POINT_PAID_ALREADY);
-            }
-        });
 
-        // 현재의 년-월 범위에 해당하는 리뷰를 조회
-        DateRange requestedDateOfMonthRange = dateFactory.getMonthRangeOf(requestDateTime);
-        List<PointProjection.ReviewAndDateDTO> reviewAndDates = pointRepository.findReviewAndDateById(
-                point.getId(),
-                requestedDateOfMonthRange.getStartDate(),
-                requestedDateOfMonthRange.getEndDate(),
-                PointCode.getReviewPointCodes()
-        );
+        // 금일 이미 목표 달성 포인트를 지급받았는지 확인
+        this.validationAchievementPointAlreadyPaid(pointLogs, requestDate);
 
-        PointProjection.ReviewAndDateDTO lastReview = reviewAndDates.get(0);
+        // 리뷰 개수가 목표 달성으로 치환되는지 확인 (목표를 달성했는지 확인)
+        PointAchievement pointAchievement = this.getPointAchievementFrom(pointLogs);
 
-        Review review = lastReview.getReview();
-        LocalDateTime lastCreatedDate = lastReview.getCreatedAt();
-        Integer count = reviewAndDates.size();
-        PointAchievement pointAchievement;
-
-        // 리뷰 개수가 목표 달성으로 치환되는지 확인
-        try {
-            pointAchievement = PointAchievement.valueOf(count);
-        } catch (InputMismatchException exception) {
-            throw new GlobalRuntimeException(PointErrorCode.ACHIEVEMENT_POINT_NOT_MATCHED);
-        }
-
-        // 목표 포인트 지급 요청 날짜가 지급받을 수 있는 날짜인지 확인
-        if (lastCreatedDate.getDayOfMonth() != requestDateTime.getDayOfMonth()) {
+        // 목표 포인트 지급 요청 날짜가 포인트를 지급받을 수 있는 날짜인지 확인
+        if (!recentPointLog.isCreated(requestDate)) {
             throw new GlobalRuntimeException(PointErrorCode.ACHIEVEMENT_POINT_PAID_FAILED);
         }
 
         // 포인트 지급
-        this.paidPoints(point, pointAchievement, review);
+        this.paidPoints(point, pointAchievement, recentPointLog.getReview());
     }
+
+    /**
+     * point의 포인트 로그중
+     * requestDateTime의 year-month에 해당하는 한달 간의 포인트 로그를 반환한다. <br>
+     * 최신 순으로 정렬
+     *
+     * @param requestDateTime LocalDateTime
+     * @param point           Point
+     * @return 포인트 로그의 리스트
+     */
+    private List<PointLog> getPointLogPerMonthBy(LocalDateTime requestDateTime, Point point) {
+
+        DateRange dateRangeOfMonth = dateFactory.getMonthRangeOf(requestDateTime);
+
+        return pointLogRepository.findDateRangeOfPointLogByPointId(
+                point.getId(), dateRangeOfMonth.getStartDate(), dateRangeOfMonth.getEndDate());
+    }
+
+    /**
+     * 최신 순으로 정렬된 PointLog 리스트 중 가장 최근의 포인트 로그를 반환한다.<br>
+     * 포인트 로그 리스트가 비어있다면 예외를 발생한다.
+     *
+     * @param pointLogs
+     * @return PointLog
+     */
+    private PointLog getRecentPointLogFrom(List<PointLog> pointLogs) throws GlobalRuntimeException {
+        return pointLogs.stream().findFirst()
+                .orElseThrow(() -> new GlobalRuntimeException(PointErrorCode.POINT_NOT_EXIST));
+    }
+
+    /**
+     * PointLog 리스트 중 requestDate에 목표달성 포인트를 지급 받은 이력이 있는지 확인한다.
+     *
+     * @param pointLogs   List
+     * @param requestDate LocalDate
+     */
+    private void validationAchievementPointAlreadyPaid(List<PointLog> pointLogs, LocalDate requestDate) {
+        pointLogs.stream()
+                .filter(pointLog -> pointLog.isCreated(requestDate))
+                .forEach(pointLog -> {
+                    if (PointAchievement.POINT_CODES.contains(pointLog.getCode())) {
+                        throw new GlobalRuntimeException(PointErrorCode.ACHIEVEMENT_POINT_PAID_ALREADY);
+                    }
+                });
+    }
+
+    /**
+     * PointLog 리스트 중 리뷰를 통해 얻은 포인트 로그의 개수를
+     * PointAchievement 객체로 치환하여 반환한다. <br>
+     * 치환이 불가능한 경우 예외를 발생한다.
+     *
+     * @param pointLogs List
+     * @return PointAchievement
+     */
+    private PointAchievement getPointAchievementFrom(List<PointLog> pointLogs) throws GlobalRuntimeException {
+        try {
+            int count = (int) pointLogs.stream().filter(pointLog -> {
+                PointCode code = pointLog.getCode();
+                return PointCode.getReviewPointCodes().contains(code);
+            }).count();
+
+            return PointAchievement.valueOf(count);
+        } catch (InputMismatchException exception) {
+            throw new GlobalRuntimeException(PointErrorCode.ACHIEVEMENT_POINT_NOT_MATCHED);
+        }
+    }
+
 
     @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
     public void paidPoints(Point point, PointAchievement pointAchievement, Review review) {
 
         Integer amount = pointAchievement.getAmount();
-
         point.plusPoint(amount);
+
         PointLog pointLog = PointLog.builder()
                 .point(point)
                 .review(review)
                 .code(pointAchievement.getPointCode())
                 .amount(pointAchievement.getAmount())
                 .build();
+
         pointLogRepository.save(pointLog);
     }
 }
