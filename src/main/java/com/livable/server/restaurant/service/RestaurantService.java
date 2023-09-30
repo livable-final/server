d80db6f7135b1d2099699be246c33a24feac7502
edan_cafe <roseswallow13@gamil.com>
2023-09-30 16:35:42 +0900

    Add: 음식점에서 판매하는 메뉴 비즈니스 로직 기능구현 (#116)

--- src/main/java/com/livable/server/restaurant/service/RestaurantService.java
+++ src/main/java/com/livable/server/restaurant/service/RestaurantService.java
@@ -1,11 +1,14 @@
 package com.livable.server.restaurant.service;
 
 import com.livable.server.core.exception.GlobalRuntimeException;
+import com.livable.server.entity.Member;
 import com.livable.server.entity.RestaurantCategory;
+import com.livable.server.member.repository.MemberRepository;
 import com.livable.server.restaurant.domain.RandomGenerator;
 import com.livable.server.restaurant.dto.RestaurantResponse;
 import com.livable.server.restaurant.repository.BuildingRestaurantMapRepository;
 import com.livable.server.restaurant.repository.RestaurantRepository;
+import com.livable.server.review.domain.ReviewErrorCode;
 import com.livable.server.visitation.domain.VisitationErrorCode;
 import com.livable.server.visitation.repository.VisitorRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -26,6 +30,7 @@ public class RestaurantService {
     private final RestaurantRepository restaurantRepository;
     private final VisitorRepository visitorRepository;
     private final BuildingRestaurantMapRepository buildingRestaurantMapRepository;
+    private final MemberRepository memberRepository;
 
     public List<RestaurantResponse.NearRestaurantDto> findNearRestaurantByVisitorIdAndRestaurantCategory(
             Long visitorId, RestaurantCategory category
@@ -43,4 +48,16 @@ public class RestaurantService {
                 buildingId, category, randomGenerator.getRandom(nearRestaurantCount)
         );
     }
-}
+
+
+    public List<RestaurantResponse.listMenuDTO> findMenuList(Long memberId, Long restaurantId) {
+        checkExistMemberById(memberId);
+
+        return restaurantRepository.findMenuList(restaurantId);
+    }
+
+    private Member checkExistMemberById(Long memberId) {
+        Optional<Member> memberOptional = memberRepository.findById(memberId);
+
+        return memberOptional.orElseThrow(() -> new GlobalRuntimeException(ReviewErrorCode.MEMBER_NOT_EXIST));
+    }}
