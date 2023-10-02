d27b2fd2c77e088129484327f12153a42dd3e125
jongy <jongyoon+u@gmail.com>
2023-10-02 13:49:35 +0900

    Feat : 룰렛 선택 결과 반영용 Service 구현 (#155)

--- src/main/java/com/livable/server/menu/service/MenuService.java
+++ src/main/java/com/livable/server/menu/service/MenuService.java
@@ -2,21 +2,33 @@ package com.livable.server.menu.service;
 
 import com.livable.server.core.exception.ErrorCode;
 import com.livable.server.core.exception.GlobalRuntimeException;
+import com.livable.server.entity.Building;
+import com.livable.server.entity.Company;
+import com.livable.server.entity.Member;
+import com.livable.server.entity.Menu;
+import com.livable.server.entity.MenuChoiceLog;
+import com.livable.server.member.domain.MemberErrorCode;
+import com.livable.server.member.repository.MemberRepository;
 import com.livable.server.menu.domain.MenuErrorCode;
+import com.livable.server.menu.dto.MenuRequest.MenuChoiceLogDTO;
 import com.livable.server.menu.dto.MenuResponse.MostSelectedMenuDTO;
 import com.livable.server.menu.dto.MenuResponse.RouletteMenuDTO;
 import com.livable.server.menu.dto.MostSelectedMenuProjection;
 import com.livable.server.menu.dto.RouletteMenu;
 import com.livable.server.menu.dto.RouletteMenuProjection;
+import com.livable.server.menu.repository.MenuChoiceLogRepository;
 import com.livable.server.menu.repository.MenuRepository;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -24,6 +36,8 @@ import org.springframework.stereotype.Service;
 public class MenuService {
 
 	private final MenuRepository menuRepository;
+	private final MenuChoiceLogRepository menuChoiceLogRepository;
+	private final MemberRepository memberRepository;
 
 	public List<RouletteMenuDTO> getRouletteMenus() {
 
@@ -79,15 +93,56 @@ public class MenuService {
 
 	private List<MostSelectedMenuDTO> convertToDTO(List<MostSelectedMenuProjection> mostSelectedMenuProjections) {
 
-	  List<MostSelectedMenuDTO> mostSelectedMenus = new ArrayList<>();
+		List<MostSelectedMenuDTO> mostSelectedMenus = new ArrayList<>();
 
-	  for (int i = 0; i < mostSelectedMenuProjections.size(); i++) {
-		int rank = i + 1;
-		MostSelectedMenuDTO mostSelectedMenuDTO = MostSelectedMenuDTO.from(mostSelectedMenuProjections.get(i), rank);
-		mostSelectedMenus.add(mostSelectedMenuDTO);
-	  }
+		for (int i = 0; i < mostSelectedMenuProjections.size(); i++) {
+		  int rank = i + 1;
+		  MostSelectedMenuDTO mostSelectedMenuDTO = MostSelectedMenuDTO.from(mostSelectedMenuProjections.get(i), rank);
+		  mostSelectedMenus.add(mostSelectedMenuDTO);
+		}
 
-	  return mostSelectedMenus;
+		return mostSelectedMenus;
 	}
-  
+
+	@Transactional
+	public void createMenuChoiceLog(Long memberId, MenuChoiceLogDTO menuChoiceLogDTO) {
+
+		Member member = memberRepository.findById(memberId)
+			.orElseThrow(() -> new GlobalRuntimeException(
+				MemberErrorCode.MEMBER_NOT_EXIST));
+
+		Company company = member.getCompany();
+
+		Building building = company.getBuilding();
+
+		Long menuId = menuChoiceLogDTO.getMenuId();
+
+	  	Menu menu = getMenu(menuId);
+
+		LocalDate date = menuChoiceLogDTO.getDate();
+
+		MenuChoiceLog menuChoiceLog = MenuChoiceLog.builder()
+			.member(member)
+			.building(building)
+			.menu(menu)
+			.date(date)
+			.build();
+
+		menuChoiceLogRepository.save(menuChoiceLog);
+
+	}
+
+	private Menu getMenu(Long menuId) {
+		return menuRepository.findById(menuId)
+			.orElseThrow(() -> new GlobalRuntimeException(
+				MenuErrorCode.MENU_NOT_EXIST)
+			);
+	}
+
+	public boolean isRouletteSelectedToday(Long memberId) {
+
+		Optional<MenuChoiceLog> menuChoiceLog = menuChoiceLogRepository.findByMemberIdAndDate(memberId, LocalDate.now());
+
+	  	return menuChoiceLog.isPresent();
+	}
 }
