7b1c6ff52a06e78cc713cec17b6a382939d07882
edan_cafe <roseswallow13@gamil.com>
2023-10-02 20:42:04 +0900

    Feat: 리뷰레포지토리 사용자 아이디 기준 리뷰 날짜 쿼리 기능 추가 (#174)

--- src/main/java/com/livable/server/review/repository/ReviewRepository.java
+++ src/main/java/com/livable/server/review/repository/ReviewRepository.java
@@ -2,6 +2,19 @@ package com.livable.server.review.repository;
 
 import com.livable.server.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query(value =
+        "SELECT count(r) " +
+        "FROM Review r " +
+        "WHERE r.member.id = :memberId and DATE(r.createdAt) = current_date "
+    )
+    Long findBymemberIdAndDate(@Param("memberId") Long memberId);
 }
