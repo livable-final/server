3ac41917ade6015c781cb2c7fc538b45f24e3d53
edan_cafe <roseswallow13@gamil.com>
2023-10-02 20:41:24 +0900

    Feat: 리뷰메뉴 레포지토리 추가 (#174)

---
+++ src/main/java/com/livable/server/review/repository/ReviewMenuMapRepository.java
@@ -0,0 +1,7 @@
+package com.livable.server.review.repository;
+
+import com.livable.server.entity.ReviewMenuMap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewMenuMapRepository extends JpaRepository<ReviewMenuMap, Long> {
+}
